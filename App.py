8ff023a1c8bf0d2fef2d781815ab1d673f0c3207
googolplex8 <googolplex8@yahoo.com>
2018-09-16 10:23:18 -0700

    not a rest API anymore

--- App.py
+++ App.py
@@ -1,34 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 import json
-
 import requests
 
 import tweet_search
 
 app = Flask(__name__)
 api = Api(app)
+keyword = ""
+attributes = [None, None, None]
+
+def setKeyword(in_keyword):
+    keyword = in_keyword
+    return "obtained keyword"
 
 
-@app.route("/keyword")
-def getKeyword(keyword):
-    return "obtain keyword"
+def setAttributes(in_attributes):
+    attributes = in_attributes
+    return "obtained attributes"
 
 
-@app.route("/attributes")
-def getAttributes(attributes):
-    return "obtain attributes"
-
-
-@app.route("/data", methods=['GET'])
 def postData():
     print("POSTING DATA")
     #API_ENDPOINT = "http:/limitless-sierra-70732.herokuapp.com/postData/"
     # r = requests.post(url=API_ENDPOINT, data=json_string)
-    data = tweet_search.TweetSearch(getKeyword(), getAttributes())
+    data = tweet_search.TweetSearch(keyword, attributes)
     json_string = json.dumps(data)
     return json_string
-  
+
 
 def index():
     return "Hello, World!"
